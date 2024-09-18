f660b640bee7bb91368b8eb22dc8daafe3c5618a
Piyush Singh <piyusshthakur5@gmail.com>
2024-09-18 13:18:05 +0530

    add to day1

---
+++ Day-1/datatypes.js
@@ -0,0 +1,11 @@
+// datatypes.js
+
+let myString = "Hello, JavaScript!";
+let isLearning = true;
+let myUndefined;
+let myNull = null;
+
+console.log(typeof myString);    // string
+console.log(typeof isLearning);  // boolean
+console.log(typeof myUndefined); // undefined
+console.log(typeof myNull);      // object (null is a special case, typeof returns 'object')
