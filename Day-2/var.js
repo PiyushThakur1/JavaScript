a3fe80c4fe27a9c851748c724ce657fd40d1fc08
Piyush Singh <piyusshthakur5@gmail.com>
2024-09-22 09:06:15 +0530

    Update

---
+++ Day-2/var.js
@@ -0,0 +1,25 @@
+let string="30 Days Of JavaScript";
+console.log(string);
+
+console.log(string.replace('JavaScript,Python'));
+// 30 Days Of JavaScript
+undefined
+console.log(string.replace('JavaScript','Python'));
+//  30 Days Of Python
+
+console.log(string.charAt(15));
+//  S
+
+console.log(string.charCodeAt(15));
+//  83
+
+console.log(string.indexOf(30));
+//  0
+
+console.log(string.indexOf("Days"));
+//  3
+
+console.log(string.indexOf("J"));
+//  11
+
+
