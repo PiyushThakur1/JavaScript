689d787dcd7ef4a24529e8646b5ac5181288b3e4
Piyush Singh <piyusshthakur5@gmail.com>
2024-12-16 23:42:15 +0530

    m

--- fr.js
+++ fr.js
@@ -1,5 +1,13 @@
-let sum=0;
-for(let i=1;i<=5;i++){
-    sum=sum+i;
+// let sum=0;
+// for(let i=1;i<=5;i++){
+//     sum=sum+i;
+// }
+// console.log("sum:-", sum);
+
+let sum = 0;
+let i = 1;
+while (i <= 5) {
+    sum = sum + i;
+    i++;
 }
 console.log("sum:-", sum);
