10da0f0eed72d9c5adbc678d7b19c747bed4f0fa
Piyush Singh <piyusshthakur5@gmail.comgit config --global user.email piyusshthakur5@gmail.com>
2024-12-20 09:58:24 +0530

    c

---
+++ DOM/tagname/script.js
@@ -0,0 +1,15 @@
+// let header = document.getElementById("heading");
+// console.dir(header);
+
+// let headings = document.getElementsByClassName("eading-class");
+// console.dir(headings);
+// console.log(headings);
+
+// let para = document.getElementsByTagName("p");
+// console.dir(para);
+
+let firstElements=document.querySelector(".heading-class");
+console.dir(firstElements);
+
+let allElements=document.querySelectorAll(".heading-class");
+console.dir(allElements);
